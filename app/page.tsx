e9357823c252869ca90b48ac904234011f60a19a
sweetman.eth <sweetmantech@gmail.com>
2024-04-23 21:09:22 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Animation from "@/components/ui/Animation";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between bg-[#586745] p-24">
+      <Animation />
+    </main>
+  );
+}
