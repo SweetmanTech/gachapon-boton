1a27c9831f1cdd52ae8b72b80b43b4787b5878a0
Sweets Sweetman <sweetmantech@gmail.com>
2024-04-23 23:01:32 -0300

    I open in same tab.

--- components/ui/ShareButton.tsx
+++ components/ui/ShareButton.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 const ShareButton = () => (
   <a
     href="https://warpcast.com/~/compose?text=el+gachapon+de+@energyonchain&embeds%5B%5D=https://gachapon.vercel.app?gachapon=true"
-    target="_blank"
     style={{
       width: 200,
       height: 200,
