87ea8778c1b9f4e4548a5d5903572f39a2d2bc48
Sweets Sweetman <sweetmantech@gmail.com>
2024-04-28 20:45:09 -0300

    I update frame to use energy gachapon.

--- components/ui/ShareButton.tsx
+++ components/ui/ShareButton.tsx
@@ -7,7 +7,7 @@ import { Address } from "viem";
 
 const ShareButton = ({ collection }: { collection: Address }) => {
   const text = "el gachapon de @energyonchain";
-  const shareUrl = `https://warpcast.com/~/compose?text=${text}&embeds%5B%5D=https://gachapon.vercel.app?collection=${collection}`;
+  const shareUrl = `https://warpcast.com/~/compose?text=${text}&embeds%5B%5D=https://gachapon-energy.vercel.app?collection=${collection}`;
   const handleClick = async (e: any) => {
     if (!(e.ctrlKey || e.metaKey)) {
       e.preventDefault();
